fa771d16e79145958b25eaf665e0e709e5ce81d4
Reth Hongti <rethhongti@onesala.com>
2021-07-14 14:01:02 +0700

    Connectivity Change

--- lib/controller/network_connection_controller.dart
+++ lib/controller/network_connection_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:connectivity/connectivity.dart';
 import 'package:get/get.dart';
 
@@ -5,19 +7,31 @@ class NetworkConnectionController extends GetxController{
   var isOnline = false.obs;
   var networkStatus = "loading".obs;
   late ConnectivityResult _connectivityResult;
+  late StreamSubscription<ConnectivityResult> _connectionStream;
+  Connectivity _connectivity = Connectivity();
 
   @override
-  void onInit() {
+  void onInit() async{
     networkChecker();
+    _connectionStream = _connectivity.onConnectivityChanged.listen(_updateNetworkStatus);
     super.onInit();
   }
 
-  void networkChecker() async{
-    _connectivityResult = await (Connectivity().checkConnectivity());
-    if(_connectivityResult == ConnectivityResult.none){
-      networkStatus.value = "No Internet Connection!!!";
-    }else{
-      networkStatus.value = "You are online.";
+  Future<dynamic> networkChecker() async{
+    _connectivityResult = await _connectivity.checkConnectivity();
+    return _updateNetworkStatus(_connectivityResult);
+  }
+  _updateNetworkStatus(ConnectivityResult result){
+    switch(result){
+      case ConnectivityResult.none:
+        networkStatus.value = "No Internet Connection!!!";
+        break;
+      case ConnectivityResult.mobile:
+        networkStatus.value = "You are online by mobile.";
+        break;
+      case ConnectivityResult.wifi:
+        networkStatus.value = "You are online by wifi.";
+        break;
     }
   }
 }
